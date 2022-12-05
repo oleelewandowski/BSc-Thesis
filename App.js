4d646861f9ef8694cbb07a5a6c67b4cb13c81646
Oliver <oliver.lewandowski1@gmail.com>
2022-12-05 18:35:17 +0100

    (feat): Demodulation v1.0

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 const SAMPLING_FREQUENCY = 44100;
 const SOUND_DURATION = 1; //sound 1s draw 10ms
-const BETA = 5;
+const BETA = 3;
 
+//INITIAL PARAMETERS
+const INITIAL_SIGNALS_AMPLITUDE = 0.5;
+// 100000 HZ BY DEFAULT - TEMP 4000
+const INITIAL_CARRIER_REAL_TIME_FREQUENCY = 4000;
+// 1000 HZ BY DEFAULT - TEMP 500
+const INITIAL_MESSAGE_REAL_TIME_FREQUENCY = 500;
+const INITIAL_SN_POWER = 15;
+//MAX FOR PLOT
+const SIGNALS_AMPLITUDE_MAX = 1;
 // SN BUTTON FLAG
 let isSNDisabled = false;
-
 //CARRIER VARIABLES
 let CARRIER_REAL_TIME_FREQUENCY;
 let CARRIER_AMPLITUDE;
@@ -16,15 +24,11 @@ let MESSAGE_ANGULAR_FREQUENCY = MESSAGE_REAL_TIME_FREQUENCY * 2 * Math.PI;
 //SN VARIABLES
 let SN_POWER;
 
-// CONSTS
-//INITIAL PARAMETERS
-const INITIAL_SIGNALS_AMPLITUDE = 0.5;
-const INITIAL_CARRIER_REAL_TIME_FREQUENCY = 2000;
-const INITIAL_MESSAGE_REAL_TIME_FREQUENCY = 200;
-const INITIAL_SN_POWER = 15;
-
-//MAX FOR PLOT
-const SIGNALS_AMPLITUDE_MAX = 1;
+// GENERATED SIGNAL SAMPLES
+let carrierData = [];
+let messageData = [];
+let modulatedData = [];
+let demodulatedData = [];
 
 //CARRIER
 const playCarrierSignal = () => {
@@ -83,7 +87,6 @@ const drawCarrierSignal = () => {
   showAxes(ctx, axes);
 
   let time = [];
-  let carrierData = [];
 
   // define plot paramaters
   let tstart = -carrierDuration;
@@ -156,7 +159,6 @@ const drawMessageSignal = () => {
   showAxes(ctx, axes);
 
   let time = [];
-  let messageData = [];
 
   // define plot paramaters
   let tstart = -messageDuration;
@@ -234,7 +236,6 @@ const drawModulatedSignal = () => {
   showAxes(ctx, axes);
 
   let time = [];
-  let modulatedData = [];
 
   // define plot paramaters
   let tstart = -modulatedDuration;
@@ -259,6 +260,175 @@ const drawModulatedSignal = () => {
   // plot function
   graphArray(ctx, axes, time, modulatedData, "rgb(255,255,255)", 1);
 };
+//--------------------------------//
+//DEMODULATION UTIL
+const demodulateSignal = (modulatedData) => {
+  const inputDemodulatorData = modulatedData;
+
+  let sin0stopni = [];
+  let sin90stopni = [];
+  let stopnie = 0;
+
+  for (let i = 0; i < inputDemodulatorData.length; i++) {
+    sin0stopni[i] = Math.sin(stopnie);
+    sin90stopni[i] = Math.cos(stopnie);
+    stopnie = stopnie + 0.4 * Math.PI;
+  }
+
+  let v_dem_sin0stopni = [];
+  let v_dem_sin90stopni = [];
+
+  for (let i = 0; i < inputDemodulatorData.length; i++) {
+    v_dem_sin0stopni[i] = inputDemodulatorData[i] * sin0stopni[i];
+    v_dem_sin90stopni[i] = inputDemodulatorData[i] * sin90stopni[i];
+  }
+
+  let v_dem_FDP_I = [];
+  let v_dem_FDP_Q = [];
+  let filtrDP = getFiltrDP(SAMPLING_FREQUENCY);
+
+  v_dem_FDP_I = convolve(v_dem_sin0stopni, filtrDP);
+  v_dem_FDP_Q = convolve(v_dem_sin90stopni, filtrDP);
+
+  let v_dem_FDP_I_diff = [];
+  let v_dem_FDP_Q_diff = [];
+
+  v_dem_FDP_I_diff[0] = v_dem_FDP_I[0];
+  v_dem_FDP_Q_diff[0] = v_dem_FDP_Q[0];
+
+  for (let i = 1; i < SAMPLING_FREQUENCY; i++) {
+    v_dem_FDP_I_diff[i] = v_dem_FDP_I[i] - v_dem_FDP_I[i - 1];
+    v_dem_FDP_Q_diff[i] = v_dem_FDP_Q[i] - v_dem_FDP_Q[i - 1];
+  }
+
+  let v_dem_FDP_I_dQ = [];
+  let v_dem_FDP_Q_dI = [];
+
+  for (let i = 0; i < SAMPLING_FREQUENCY; i++) {
+    v_dem_FDP_I_dQ[i] = v_dem_FDP_I[i] * v_dem_FDP_Q_diff[i];
+    v_dem_FDP_Q_dI[i] = v_dem_FDP_Q[i] * v_dem_FDP_I_diff[i];
+  }
+
+  let v_dem_syg_roznicowy = [];
+
+  for (let i = 0; i < SAMPLING_FREQUENCY; i++) {
+    v_dem_syg_roznicowy[i] = v_dem_FDP_I_dQ[i] - v_dem_FDP_Q_dI[i];
+  }
+
+  console.log("v_dem_FDP_I_dQ", v_dem_FDP_I_dQ);
+  console.log("v_dem_FDP_Q_dI", v_dem_FDP_Q_dI);
+
+  return v_dem_syg_roznicowy;
+};
+const getFiltrDP = (sampFreq) => {
+  let p = -sampFreq / 2;
+  let filtrDP = [];
+  for (let i = 0; i < sampFreq; i++) {
+    if (p === 0) filtrDP[i] = 1;
+    else filtrDP[i] = Math.sin(p) / p;
+    p = p + 1;
+  }
+  return filtrDP;
+};
+const convolve = (vec1, vec2) => {
+  if (vec1.length === 0 || vec2.length === 0) {
+    throw new Error("Vectors can not be empty!");
+  }
+  const firstVector = vec1;
+  const secondVector = vec2;
+  let displacement = 0;
+  const convVec = [];
+  let convVecSame = [];
+
+  for (let i = 0; i < firstVector.length; i++) {
+    for (let j = 0; j < secondVector.length; j++) {
+      if (displacement + j !== convVec.length) {
+        convVec[displacement + j] =
+          convVec[displacement + j] + firstVector[i] * secondVector[j];
+      } else {
+        convVec.push(firstVector[i] * secondVector[j]);
+      }
+    }
+    displacement++;
+  }
+
+  let helper = 0;
+  for (let i = SAMPLING_FREQUENCY / 2; i < (SAMPLING_FREQUENCY / 2) * 3; i++) {
+    convVecSame[helper] = convVec[i];
+    helper++;
+  }
+
+  return convVecSame;
+};
+const drawDemodulatedSignal = () => {
+  let demodulatedDuration = SOUND_DURATION / 100;
+
+  // define canvas
+  let canvas = document.querySelector("#my-demodulated-canvas");
+  if (null === canvas || !canvas.getContext) return 0;
+  ctx = canvas.getContext("2d");
+
+  //clear rect if function called
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // axes center coordinates
+  let axes = {};
+
+  axes.x0 = 0.5 * canvas.width;
+  axes.y0 = 0.5 * canvas.height;
+
+  showAxes(ctx, axes);
+
+  let time = [];
+
+  // define plot paramaters
+  let tstart = -demodulatedDuration;
+  let tstop = demodulatedDuration; //10ms *2
+  let dt = (tstop - tstart) / SAMPLING_FREQUENCY; // time increment
+
+  axes.xscale = canvas.width / (2 * demodulatedDuration); // x pixs per s
+  axes.yscale = canvas.height / (2 * SIGNALS_AMPLITUDE_MAX); // y pixs per V
+  axes.sampFreq = SAMPLING_FREQUENCY;
+
+  let demodulatedDataArray = demodulateSignal(modulatedData);
+
+  for (let sample = 0; sample < SAMPLING_FREQUENCY; sample++) {
+    time[sample] = tstart + sample * dt;
+    demodulatedData[sample] = Math.pow(10, 6) * demodulatedDataArray[sample];
+  }
+
+  // plot function
+  graphArray(ctx, axes, time, demodulatedData, "rgb(255,255,255)", 1);
+};
+const playDemodulatedSignal = () => {
+  const ALL_SAMPLES = SOUND_DURATION * SAMPLING_FREQUENCY;
+  const CHANNEL_TYPE = 1; //1-mono, 2-stereo
+
+  let audioContext = new AudioContext();
+
+  //CHANNEL_TYPE - mono channel <--> ALL_SAMPLES - samples the buffer should hold <--> SAMPLING_FREQUENCY - sample rate of the buffer
+  let buffer = audioContext.createBuffer(
+    CHANNEL_TYPE,
+    ALL_SAMPLES,
+    SAMPLING_FREQUENCY
+  );
+
+  let data = buffer.getChannelData(0);
+
+  function generateSample(sample) {
+    return CARRIER_AMPLITUDE * demodulatedData[sample];
+  }
+
+  for (let sample = 0; sample < ALL_SAMPLES; sample++) {
+    data[sample] = generateSample(sample);
+  }
+
+  let source = audioContext.createBufferSource();
+  source.buffer = buffer;
+  source.connect(audioContext.destination);
+  source.start();
+};
+//--------------------------------//
 //UTIL FOR FUNCTIONS DRAWING
 const graphArray = (ctx, axes, t, data, color, thick) => {
   let t0 = axes.x0;
@@ -296,7 +466,6 @@ const showAxes = (ctx, axes) => {
   ctx.lineTo(x0, height); // Y axis
   ctx.stroke();
 };
-
 //S/N RATIO HANDLER
 const handleSNOnInput = (newValue) => {
   //get dom element
@@ -311,7 +480,6 @@ const handleSNOnInput = (newValue) => {
   //overwrite sn power
   SN_POWER = enteredValue;
 };
-
 const handleSNButton = () => {
   handleSNOnInput(INITIAL_SN_POWER);
   //button text and flag handler
@@ -336,7 +504,6 @@ const handleSNButton = () => {
     inputElement.disabled = false;
   }
 };
-
 //CONTROL PANEL AMPLITUDE, FREQ CHANGE
 const handleCarrierAmplitudeChange = (newValue) => {
   //get dom elements
@@ -353,6 +520,7 @@ const handleCarrierAmplitudeChange = (newValue) => {
   //draw signals
   drawCarrierSignal();
   drawModulatedSignal();
+  drawDemodulatedSignal();
 };
 const handleCarrierFreqChange = (newValue) => {
   //get dom elements
@@ -370,6 +538,7 @@ const handleCarrierFreqChange = (newValue) => {
   //draw signals
   drawCarrierSignal();
   drawModulatedSignal();
+  drawDemodulatedSignal();
 };
 const handleMessageAmplitudeChange = (newValue) => {
   //get dom elements
@@ -386,6 +555,7 @@ const handleMessageAmplitudeChange = (newValue) => {
   //draw signals
   drawMessageSignal();
   drawModulatedSignal();
+  drawDemodulatedSignal();
 };
 const handleMessageFreqChange = (newValue) => {
   //get dom elements
@@ -403,6 +573,7 @@ const handleMessageFreqChange = (newValue) => {
   //draw signals
   drawMessageSignal();
   drawModulatedSignal();
+  drawDemodulatedSignal();
 };
 
 //INITIAL START / RESET TO DEFAULT PARAMS
@@ -416,4 +587,5 @@ const startAppWithDefaultParameters = () => {
   drawCarrierSignal();
   drawMessageSignal();
   drawModulatedSignal();
+  drawDemodulatedSignal();
 };
